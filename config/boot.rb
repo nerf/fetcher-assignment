a6fe7ba4e1e3f955a1e6c017528ba88fd859841a
Rosen Tihomirov <rosko@mindsys.org>
2020-08-23 14:49:48 +0300

    Load sub directories

--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,6 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(env.to_sym)
 
-Dir["./app/{lib,models,services}/*.rb"].each { |file| require file }
+Dir["./app/{lib,models,services}/**/*.rb"].each { |file| require file }
 
 require './app/controllers/application'
