4e788b5f4dafae714be52f3ce9403661e6d7f59c
Rosen Tihomirov <rosko@mindsys.org>
2020-08-23 21:40:28 +0300

    Implement LibrariesFetcher.call

--- app/services/libraries_fetcher.rb
+++ app/services/libraries_fetcher.rb
@@ -1,5 +1,5 @@
 class LibrariesFetcher
-  class Result
+  class Results
     def sort_by(atr)
       self
     end
@@ -7,9 +7,18 @@ class LibrariesFetcher
     def as_json(args)
       []
     end
+
+    def <<(list)
+    end
   end
 
-  def self.call(using:, filters: {})
-    Result.new
+  def self.call(using: [], lang: nil)
+    results = Results.new
+
+    using.each do |fetcher|
+      results << fetcher.latest_public_libraries(lang: lang)
+    end
+
+    results
   end
 end
--- spec/services/libraries_fetcher_spec.rb
+++ spec/services/libraries_fetcher_spec.rb
@@ -1,3 +1,19 @@
 RSpec.describe LibrariesFetcher do
+  describe '#call' do
+    let(:results) { described_class.call(using: [Fetch::Gitlab]) }
 
+    before do
+      allow(Fetch::Gitlab).to receive(:latest_public_libraries)
+
+      results
+    end
+
+    it 'executes listed fetchers' do
+      expect(Fetch::Gitlab).to have_received(:latest_public_libraries).with(lang: nil).once
+    end
+
+    it 'returns "results" object' do
+      expect(results).to be_kind_of(described_class::Results)
+    end
+  end
 end
