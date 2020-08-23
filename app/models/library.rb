f7db1c227142f47eb8cc561638445e6423e5259c
Rosen Tihomirov <rosko@mindsys.org>
2020-08-23 22:27:20 +0300

    Implement Library#as_json

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,3 +1,11 @@
 class Library < Struct.new(:url, :username, :name, :description, :source, :updated_at, keyword_init: true)
-
+  def as_json(only: nil)
+    if only
+      only.each_with_object({}) do |atr, obj|
+        obj[atr] = public_send(atr)
+      end
+    else
+      to_h
+    end
+  end
 end
--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -1,8 +1,27 @@
 RSpec.describe Library do
-  it 'can be initiated using keywords' do
-    library = Library.new name: 'Foo', username: 'Bar'
+  let(:library) { Library.new name: 'Foo', username: 'Bar' }
 
-    expect(library.name).to eq('Foo')
-    expect(library.username).to eq('Bar')
+  describe '.new' do
+    it 'can be initiated using keywords' do
+      expect(library.name).to eq('Foo')
+      expect(library.username).to eq('Bar')
+    end
+  end
+
+  describe '#as_json' do
+    it 'returns hash of object data' do
+      expected_result = {
+        url: nil, username: 'Bar', name: 'Foo', description: nil,
+        source: nil, updated_at: nil
+      }
+
+      expect(library.as_json).to match(expected_result)
+    end
+
+    it 'can filter returned data with only keyword' do
+      expected_result = { name: 'Foo', url: nil }
+
+      expect(library.as_json(only: %i(name url))).to match(expected_result)
+    end
   end
 end
