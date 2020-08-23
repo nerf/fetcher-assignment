99c66954eea5b6061d6706174107e373356fd7d3
Rosen Tihomirov <rosko@mindsys.org>
2020-08-23 14:35:51 +0300

    Make use of LibrariesFetcher (pseudocode)

--- app/controllers/libraries.rb
+++ app/controllers/libraries.rb
@@ -1,6 +1,12 @@
 class Libraries < Sinatra::Base
   get '/libraries' do
-    # TODO: return list of fetched repositories
-    { data: [] }.to_json
+    lang = params[:language]
+
+    results = LibrariesFetcher
+                .call(using: %i(gitlab github), filters: { language: lang })
+                .sort_by(:updated_at)
+                .as_json(only: %i(url owner name description host))
+
+    { data: results }.to_json
   end
 end
