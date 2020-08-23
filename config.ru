0bac277a84845445cd788f3c4e51a6143d7b2696
Rosen Tihomirov <rosko@mindsys.org>
2020-08-24 01:49:40 +0300

    Configure rack cache

--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
 require './config/boot'
 
+use Rack::Cache,
+    verbose: true,
+    metastore: 'memcached://localhost',
+    entitystore: 'memcached://localhost'
+
 run Application
--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,8 @@ env = ENV['RACK_ENV'] ||= 'production'
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(env.to_sym)
+require 'dalli'
+require 'rack-cache'
 
 Dir["./app/{lib,models,services}/**/*.rb"].each { |file| require file }
 
